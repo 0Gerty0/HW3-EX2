f3ae5c11b3f36e4b2be9a7c490e8ee2106788757
Aleksandr Danilushkin <thenumber23@ya.ru>
2024-04-11 19:30:39 +0300

    11_04_24

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate (int weightKg, double heightMeter){
-        double bmi = weightKg/ Math.pow (heightMeter,2);
+    public int calculate(int weightKg, double heightMeter) {
+        double bmi = weightKg / Math.pow(heightMeter, 2);
         return (int) bmi;
     }
 }
